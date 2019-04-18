4bdbe95859e9ca60e4ad2c804746c6ff1a024796
Fernando Aguate <fmaguate@gmail.com>
2019-04-18 10:21:16 -0400

    Update code_urls.R

--- code_urls.R
+++ code_urls.R
@@ -1,4 +1,4 @@
 code_urls <- c(
   'https://raw.githubusercontent.com/FerAguate/SmallRFunctions/master/create_color_vector.R',
-  'https://github.com/FerAguate/mybiplot/master/mybiplot.R'
+  'https://raw.githubusercontent.com/FerAguate/mybiplot/master/mybiplot.R'
   )
