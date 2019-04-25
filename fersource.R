144314dd83a324fc3b5b5807fa066e1997034716
Fernando Aguate <fmaguate@gmail.com>
2019-04-25 09:47:43 -0400

    Update fersource.R

--- fersource.R
+++ fersource.R
@@ -1,8 +1,9 @@
 fersource <- function(myfunction){
   for(fun in myfunction){
   eval(parse(text = RCurl::getURL('https://raw.githubusercontent.com/FerAguate/SmallRFunctions/master/code_urls.R', ssl.verifypeer = FALSE)))
-  myfunction_pos <- grep(paste0('^', fun, '$'), sapply(strsplit(code_urls, '/master/'), function(x) x[2]), ignore.case = T)[1]
-  if(length(myfunction_pos) == 0) stop('Function not found')
+  functions <- gsub('.R', '', sapply(strsplit(code_urls, '/master/'), function(x) x[2]))
+  myfunction_pos <- grep(paste0('^', fun, '$'), functions, ignore.case = T)[1]
+  if(is.na(myfunction_pos)) stop('Not found. Functions are: ', paste(functions, collapse = ' '))
   text_function <- RCurl::getURL(code_urls[myfunction_pos], ssl.verifypeer = FALSE)
   eval(parse(text = text_function), envir = globalenv())
   }
