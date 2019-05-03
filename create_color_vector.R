80f97a8849f2f80e8802ad4687f295eab19d10e7
Fernando Aguate <fmaguate@gmail.com>
2019-05-03 11:26:16 -0400

    Update create_color_vector.R

--- create_color_vector.R
+++ create_color_vector.R
@@ -1,3 +1,4 @@
 create_color_vector <- function(vector, colors = c('#1B9E77', '#D95F02')){
+  if(length(colors) < length(unique(vector))) colors <- rainbow(length(unique(vector)), s = .9)
   as.character(factor(vector, levels = unique(vector), labels = colors))  
 }
