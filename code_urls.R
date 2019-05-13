abe4a13d9a176312b1b74d9fd9d906ef6d1c0825
Fernando Aguate <fmaguate@gmail.com>
2019-05-13 16:09:38 -0400

    Update code_urls.R

--- code_urls.R
+++ code_urls.R
@@ -1,6 +1,6 @@
 code_urls <- c(
   'https://raw.githubusercontent.com/FerAguate/SmallRFunctions/master/create_color_vector.R',
+  'https://raw.githubusercontent.com/FerAguate/SmallRFunctions/master/load_get.R',
   'https://raw.githubusercontent.com/FerAguate/mybiplot/master/mybiplot.R',
-  'https://raw.githubusercontent.com/FerAguate/OrthogonalBranches/master/grow_tree.R',
-  'https://raw.githubusercontent.com/FerAguate/OrthogonalBranches/master/load_get.R'
+  'https://raw.githubusercontent.com/FerAguate/OrthogonalBranches/master/grow_tree.R'
   )
