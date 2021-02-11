3ad3cbf0bdabaa2f442fbb338714cd5c6a33ef71
Fernando Aguate <fmaguate@gmail.com>
2021-02-11 13:19:21 -0500

    Update load_from_rdata.R

--- load_from_rdata.R
+++ load_from_rdata.R
@@ -1,8 +1,8 @@
 #' @title load_from_rdata
 #' @description Loads one object from a RData file.
 #' @usage load_get(f, ...)
-#' @arguments f = a connection or a character string giving the name of the file to load.
-#' @arguments index = integer indicating the position of the object to load. By default = 1 (reference to the first object in the RData file).
+#' @param f a connection or a character string giving the name of the file to load.
+#' @param index integer indicating the position of the object to load. By default = 1 (reference to the first object in the RData file).
 
 load_from_rdata <- function(f, index = 1){
   env <- new.env()
