b076ee1841c4b011c9c2f950dacdbb462fa87ca9
Fernando Aguate <fmaguate@gmail.com>
2021-02-11 13:12:18 -0500

    Update code_urls.R

--- code_urls.R
+++ code_urls.R
@@ -1,6 +1,6 @@
 code_urls <- c(
   'https://raw.githubusercontent.com/FerAguate/SmallRFunctions/master/create_color_vector.R',
-  'https://raw.githubusercontent.com/FerAguate/SmallRFunctions/master/load_get.R',
+  'https://raw.githubusercontent.com/FerAguate/SmallRFunctions/master/load_from_rdata.R',
   'https://raw.githubusercontent.com/FerAguate/mybiplot/master/mybiplot.R',
   'https://raw.githubusercontent.com/FerAguate/OrthogonalBranches/master/grow_tree.R',
   'https://raw.githubusercontent.com/FerAguate/SmallRFunctions/master/add_legend.R'
