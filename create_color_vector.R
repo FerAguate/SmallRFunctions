d4bd7a886e1ceaef7c214a4b44b5b6f8b6daacf7
Fernando Aguate <fmaguate@gmail.com>
2021-02-11 13:19:37 -0500

    Update create_color_vector.R

--- create_color_vector.R
+++ create_color_vector.R
@@ -1,8 +1,8 @@
 #' @title create_color_vector
 #' @description Creates a vector of colors matching each category of the imput vector.
 #' @usage create_color_vector(vector, ...)
-#' @param vector = a vector of numbers or characters with more than one category.
-#' @param colors = colors for each category in increasing order.
+#' @param vector a vector of numbers or characters with more than one category.
+#' @param colors colors for each category in increasing order.
 #' @details Function useful when adding colors by category in the base function 'plot'.
 #' @example x <- rnorm(1000)
 #' @example plot(x, col = create_color_vector(x > 0))
