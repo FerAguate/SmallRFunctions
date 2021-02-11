f2e25a2dddf7950e644273e79c78f3545b297a5d
Fernando Aguate <fmaguate@gmail.com>
2021-02-11 13:22:28 -0500

    Update create_color_vector.R

--- create_color_vector.R
+++ create_color_vector.R
@@ -4,8 +4,9 @@
 #' @param vector a vector of numbers or characters with more than one category.
 #' @param colors colors for each category in increasing order.
 #' @details Function useful when adding colors by category in the base function 'plot'.
-#' @example x <- rnorm(1000)
-#' @example plot(x, col = create_color_vector(x > 0))
+#' @examples 
+#' x <- rnorm(1000)
+#' plot(x, col = create_color_vector(x > 0))
 #' @author Fernando Aguate
 create_color_vector <- function(vector, colors = c('#1B9E77', '#D95F02')){
   if(length(colors) < length(unique(vector))) colors <- rainbow(length(unique(vector)), s = .9)
