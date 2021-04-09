de16d2f19d0010bf3e8301188ceb9cf0ecb0d554
Fernando Aguate <fmaguate@gmail.com>
2021-04-09 09:59:04 -0400

    Update code_urls.R

--- code_urls.R
+++ code_urls.R
@@ -3,5 +3,6 @@ code_urls <- c(
   'https://raw.githubusercontent.com/FerAguate/SmallRFunctions/master/load_from_rdata.R',
   'https://raw.githubusercontent.com/FerAguate/mybiplot/master/mybiplot.R',
   'https://raw.githubusercontent.com/FerAguate/OrthogonalBranches/master/grow_tree.R',
-  'https://raw.githubusercontent.com/FerAguate/SmallRFunctions/master/add_legend.R'
+  'https://raw.githubusercontent.com/FerAguate/SmallRFunctions/master/add_legend.R',
+  'https://raw.githubusercontent.com/FerAguate/SmallRFunctions/master/myheatLD.R'
   )
